3501e67af26c8ae85fcf3ededade96d7292814c8
Aleaume <aleaume.cousseau@adeeqp.onmicrosoft.com>
2022-12-22 14:14:55 +0100

    1st draft of train py NOT TESTED

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -1,24 +1,106 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import re
+import nltk
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import GridSearchCV
 
 def load_data(database_filepath):
-    pass
+
+    # load data from database
+    engine = create_engine('sqlite://{}'.format(database_filepath))
+    df = pd.read_sql_table('Messages_cleaned',engine)
+
+    # X represents the independent variables, or the features, that are used to predict the output variable Y.
+    X= df[['message']]
+
+    #Y, on the other hand, represents the dependent variable, or the target, that the model is trying to predict.
+    Y = df.drop(columns=['id','message','original','genre'])
+
+    #retrieve category_names
+    category_names = Y.columns
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # Tokenize text
+    words = word_tokenize(text)
+
+    # Remove stop words
+    words = [w for w in words if w not in stopwords.words("english")]
+
+    # Reduce words to their stems
+    words = [PorterStemmer().stem(w) for w in words]
+
+    # Reduce words to their root form
+    words = [WordNetLemmatizer().lemmatize(w) for w in words]
+
+    return words
 
 
 def build_model():
-    pass
+
+    # build pipeline
+
+    pipeline = Pipeline([
+        ('vect',CountVectorizer(tokenizer=tokenize)),
+        ('tfidf',TfidfTransformer()),
+        ('clf',MultiOutputClassifier(RandomForestClassifier())),
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    predicted = model.predict(X_test['message'])
+    labels = np.unique(predicted)
+
+    i=0
+    for col in category_names:
+
+        class_report = classification_report(Y_test[col].to_numpy(), predicted[:,i],labels=labels,output_dict=True)
+        sns.heatmap(pd.DataFrame(class_report).iloc[:-1, :].T, annot=True)
+
+        tn, fp, fn, tp = confusion_matrix(Y_test[col].to_numpy(), predicted[:,i],labels=labels).ravel()
+        print("Target: {}\n-----------\nTrue Negative: {}\nFalse Positive: {}\nFalse Negative: {}\nTrue Positive: {}\n\n".format(col,tn, fp, fn, tp))
+        cm = confusion_matrix(Y_test[col].to_numpy(), predicted[:,i],labels=labels)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
+        plt.show()
+        i+=1
+        
+        
+        
     pass
 
 
 def save_model(model, model_filepath):
-    pass
+    #saving model
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
