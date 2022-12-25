4a3259fbad74eab9995ca8d9a79e69a39b500dbc
Aleaume <aleaume.cousseau@adeeqp.onmicrosoft.com>
2022-12-25 22:17:38 +0100

    added if exists replace on DB table creation

--- workspace/data/process_data.py
+++ workspace/data/process_data.py
@@ -87,7 +87,7 @@ def save_data(df, database_filename):
     """
     #Save the clean dataset into an sqlite database.
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('Messages_cleaned', engine, index=False)
+    df.to_sql('Messages_cleaned', engine, index=False, if_exists='replace')
 
     pass  
 
